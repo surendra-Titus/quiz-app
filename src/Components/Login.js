6d0ed403b51ea287f73d6319bde2b221f56d629d
Naga Surendra <naga.surendra@mariapps.com>
2022-05-03 11:36:14 +0530

    Initial SetUp

---
+++ src/Components/Login.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Button, TextField } from '@mui/material'
+export default function Login() {
+  return (
+      <form noValidate autoComplete='off'>
+          <TextField
+          label="Email"
+          name='email'
+          variant='outlined'/>
+          <TextField
+          label="Name"
+          name='name'
+          variant='outlined'/>
+          <Button
+          type='submit'
+          variant='contained'
+          size='large'>Start</Button>
+      </form>
+  )
+}
